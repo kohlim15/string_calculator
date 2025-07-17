341b161bfaaf5562e43f3204dbc1c231aae1ddc0
Kunal <kohlim15@gmail.com>
2025-07-17 18:44:39 +0530

    Test case for returning number itself

--- test_string_calculator.rb
+++ test_string_calculator.rb
@@ -5,4 +5,8 @@ class StringCalculatorTest < Minitest::Test
   def test_empty_string_returns_0
     assert_equal 0, StringCalculator.add("")
   end
+
+  def test_single_number_returns_itself
+    assert_equal 5, StringCalculator.add("5")
+  end
 end
