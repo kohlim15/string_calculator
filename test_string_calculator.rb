eb568959a77b18659b506ddede00b9f9693e2cf0
Kunal <kohlim15@gmail.com>
2025-07-17 19:05:50 +0530

    Test case to handle new line between numbers

--- test_string_calculator.rb
+++ test_string_calculator.rb
@@ -7,16 +7,20 @@ class StringCalculatorTest < Minitest::Test
   end
 
   def test_single_number_returns_itself
-    assert_equal 5, StringCalculator.add("5")
+    assert_equal 1, StringCalculator.add("1")
   end
 
   def test_two_numbers_comma_delimited
-    assert_equal 3, StringCalculator.add("1,2")
+    assert_equal 6, StringCalculator.add("1,5")
   end
 
   def test_any_amount_of_numbers
     numbers = (1..100).to_a.join(",")
     expected_total = (1..100).sum
     assert_equal expected_total, StringCalculator.add(numbers)
+  end
+
+  def test_new_line_between_numbers
+    assert_equal 6, StringCalculator.add("1\n2,3")
   end
 end
