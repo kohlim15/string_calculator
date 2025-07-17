218fb5cf1e1ec36d2394f705cfe221dbe4030c1c
Kunal <kohlim15@gmail.com>
2025-07-17 19:02:36 +0530

    Test case for handling any amount of numbers

--- test_string_calculator.rb
+++ test_string_calculator.rb
@@ -13,4 +13,10 @@ class StringCalculatorTest < Minitest::Test
   def test_two_numbers_comma_delimited
     assert_equal 3, StringCalculator.add("1,2")
   end
+
+  def test_any_amount_of_numbers
+    numbers = (1..100).to_a.join(",")
+    expected_total = (1..100).sum
+    assert_equal expected_total, StringCalculator.add(numbers)
+  end
 end
