35a5e90a5640af55b700003e0f507157065d7d90
Kunal <kohlim15@gmail.com>
2025-07-18 15:47:01 +0530

    Added logic to handle custom delimiter

--- string_calculator.rb
+++ string_calculator.rb
@@ -1,6 +1,14 @@
 class StringCalculator
   def self.add(numbers)
     return 0 if numbers.empty?
-    numbers.split(/,|\n/).map(&:to_i).sum
+
+    delimiter = /,|\n/
+    if numbers.start_with?("//")
+      header, numbers = numbers.split("\n", 2)
+      custom_delim = Regexp.escape(header[2])
+      delimiter = /#{custom_delim}/
+    end
+
+    numbers.split(delimiter).map(&:to_i).sum
   end
 end
